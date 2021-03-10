496a6ffe5a1c08cb5271cc831e5be0a9fcb6f9f7
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 00:49:53 +0100

    Create index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+fanction main () {
+ // Only change code below this line
+   var product;
+   var numOne = 15;
+   var numTwo = 3;
+   difference = numOne * numTwo;
+ // Only change code below this line
+  return product;
+}
+
+console.log(main(15, 3));
+
+module.exports = main;
