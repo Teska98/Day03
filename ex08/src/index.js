507ecf97a8b934fcd850d920f1dfedde7d6bf511
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 00:43:32 +0100

    Create index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+fanction main () {
+ // Only change code below this line
+ var numOne = 15;
+ var numTwo = 8;
+ var sum;
+ sum = numOne + numTwo; 
+ // Only change code below this line
+  return sum;
+  
+  }
+}
+
+console.log(main(15,8));
+
+module.exports = main;
