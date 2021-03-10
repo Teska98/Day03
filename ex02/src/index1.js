1d323b7af7ea4b0df0bb45611bad46f18552094c
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-10 23:56:17 +0100

    Create index1.js

---
+++ ex02/src/index1.js
@@ -0,0 +1,14 @@
+fanction main () {
+ // Only change code below this line
+     //I am Edvin
+     /*I am Edvin;
+     I have 23 years;
+     I love swimming;
+     */
+ // Only change code below this line
+ 
+}
+
+main();
+
+module.exports = main;
