ade20cdfe712ce0f0f1f859bfd43ab92c43bf966
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-11 00:30:58 +0100

    Create index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+fanction main () {
+ // Only change code below this line
+ // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+ //Vriable assigments
+    var studlyCapVar = 10;
+    var properCamelCase = "A String";
+    var titleCaseOver = 9000;
+ // Only change code below this line
+  return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver,
+  }
+}
+
+main();
+
+module.exports = main;
